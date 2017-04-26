5f55f48181253b904522af08c3452a9942a6bc88
Christopher Young <cyoung@gmail.com>
2017-04-25 23:05:19 -0400

    SPI bulk read/write functions.

--- goRFM95W/main.go
+++ goRFM95W/main.go
@@ -1,7 +1,6 @@
 package goRFM95W
 
 import (
-	"fmt"
 	"github.com/cyoung/rpi"
 	"golang.org/x/exp/io/spi"
 	"time"
@@ -71,28 +70,48 @@ func (r *RFM95W) Close() {
 	r.SPI.Close()
 }
 
-func (r *RFM95W) GetRegister(reg byte) (byte, error) {
+func (r *RFM95W) GetBytes(reg byte, len int) ([]byte, error) {
 	rpi.DigitalWrite(RF95W_CS_PIN, rpi.LOW)
 	defer rpi.DigitalWrite(RF95W_CS_PIN, rpi.HIGH)
 
-	buf := make([]byte, 2)
+	buf := make([]byte, len+1)
 	err := r.SPI.Tx([]byte{byte(uint32(reg) & ^uint32(SPI_WRITE_MASK)), 0x00}, buf)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
-	return buf[1], nil
+	return buf[1:], nil
+}
+
+func (r *RFM95W) SetBytes(reg byte, val []byte) ([]byte, error) {
+	rpi.DigitalWrite(RF95W_CS_PIN, rpi.LOW)
+	defer rpi.DigitalWrite(RF95W_CS_PIN, rpi.HIGH)
+
+	outBuf := []byte{reg | SPI_WRITE_MASK}
+	outBuf = append(outBuf, val...)
+	inBuf := make([]byte, 1+len(val))
+
+	err := r.SPI.Tx(outBuf, inBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	return inBuf[1:], nil
+}
+
+func (r *RFM95W) GetRegister(reg byte) (byte, error) {
+	var ret byte
+	x, err := r.GetBytes(reg, 1)
+	if x != nil {
+		ret = x[0]
+	}
+	return ret, err
 }
 
 func (r *RFM95W) SetRegister(reg, val byte) (byte, error) {
-	rpi.DigitalWrite(RF95W_CS_PIN, rpi.LOW)
-	defer rpi.DigitalWrite(RF95W_CS_PIN, rpi.HIGH)
-
-	buf := make([]byte, 2)
-	err := r.SPI.Tx([]byte{reg | SPI_WRITE_MASK, val}, buf)
-	if err != nil {
-		return 0, err
+	var ret byte
+	x, err := r.SetBytes(reg, []byte{val})
+	if x != nil {
+		ret = x[0]
 	}
-
-	fmt.Printf("%v\n", buf)
-	return buf[1], nil
+	return ret, err
 }
