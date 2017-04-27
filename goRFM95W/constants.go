ffc2ea9096dd9d72a9468fe3affc4edb89aa02ab
Christopher Young <cyoung@gmail.com>
2017-04-27 12:41:23 -0400

    Continuous read mode. Queue handler goroutine.

--- goRFM95W/constants.go
+++ goRFM95W/constants.go
@@ -12,4 +12,28 @@ const (
 	RF95W_MODE_CAD          = 0x07
 
 	RF95W_FREQ_STEP = 32000000.0 / 524288.0 // 32 MHz oscillator, 2^19 bits. ~61 Hz.
+
+	RF95W_IRQ_FLAG_RXTIMEOUT         = 0x80
+	RF95W_IRQ_FLAG_RXDONE            = 0x40
+	RF95W_IRQ_FLAG_PAYLOADCRCERROR   = 0x20
+	RF95W_IRQ_FLAG_VALIDHEADER       = 0x10
+	RF95W_IRQ_FLAG_TXDONE            = 0x08
+	RF95W_IRQ_FLAG_CADDONE           = 0x04
+	RF95W_IRQ_FLAG_FHSSCHANGECHANNEL = 0x02
+	RF95W_IRQ_FLAG_CADDETECTED       = 0x01
+
+	MAX_TXQUEUE_PILEUP = 100000 // About 25MB of messages. Start dropping messages in the queue once reaching this.
 )
+
+// bandwidth (Hz) -> setting.
+var RFM95W_Bandwidths = map[int]byte{
+	7800:   0x0,
+	10400:  0x1,
+	15600:  0x2,
+	20800:  0x3,
+	31250:  0x4,
+	41700:  0x5,
+	62500:  0x6,
+	125000: 0x7,
+	500000: 0x8,
+}
--- goRFM95W/main.go
+++ goRFM95W/main.go
@@ -1,6 +1,8 @@
 package goRFM95W
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"github.com/cyoung/rpi"
@@ -10,8 +12,8 @@ import (
 
 type RFM95W_Message struct {
 	Buf      []byte
-	RSSI     int
-	SNR      int
+	RSSI     int     // dBm
+	SNR      float64 // dB
 	Received time.Time
 }
 
@@ -25,6 +27,7 @@ type RFM95W struct {
 	pr            int
 	interruptChan chan int
 	RecvBuf       []RFM95W_Message // This is constantly being filled up as messages are received.
+	txQueue       chan []byte
 	currentMode   byte
 }
 
@@ -77,6 +80,8 @@ func New() (*RFM95W, error) {
 		txpwr: RF85W_DEFAULT_TXPWR,
 	}
 
+	ret.txQueue = make(chan []byte, 1024)
+
 	time.Sleep(100 * time.Millisecond)
 
 	err = ret.init()
@@ -90,6 +95,14 @@ func (r *RFM95W) SetMode(mode byte) error {
 		r.currentMOde = mode
 	}
 	return err
+}
+
+func (r *RFM95W) GetMode() (byte, error) {
+	ret, err := r.GetRegister(0x01)
+	if err != nil {
+		currentMode = ret
+	}
+	return ret, err
 }
 
 /*
@@ -143,19 +156,6 @@ func (r *RFM95W) init() error {
 	SetBandwidth().
 	 Sets the total bandwidth to use in the transmission.
 */
-
-// bandwidth (Hz) -> setting.
-var RFM95W_Bandwidths = map[int]byte{
-	7800:   0x0,
-	10400:  0x1,
-	15600:  0x2,
-	20800:  0x3,
-	31250:  0x4,
-	41700:  0x5,
-	62500:  0x6,
-	125000: 0x7,
-	500000: 0x8,
-}
 
 func (r *RFM95W) SetBandwidth(bw int) error {
 	if b, ok := RFM95W_Bandwidths[bw]; !ok {
@@ -272,7 +272,7 @@ func (r *RFM95W) SetTXPower() error {
 
 /*
 	Send().
-	 Sends a single message. Stops the receive thread and switches to TX mode until the message is sent.
+	 Sends a single message. Sends the message to the txQueue channel for processing by queueHandler.
 */
 
 func (r *RFM95W) Send(msg []byte) error {
@@ -280,8 +280,10 @@ func (r *RFM95W) Send(msg []byte) error {
 		return errors.New("Message too long.")
 	}
 
-	//FIXME: Stop the receive thread.
+	r.txQueue <- msg
+}
 
+func (r *RFM95W) sendMessage(msg []byte) error {
 	r.SetMode(RF95W_MODE_STDBY)
 
 	// Set the FIFO address pointer to the start.
@@ -307,7 +309,112 @@ func (r *RFM95W) Send(msg []byte) error {
 	return err
 }
 
-//TODO
+//TODO: Put the receiver into STDBY when SetFrequency, etc are called.
 func (r *RFM95W) queueHandler() {
+	//FIXME: Assuming that we're ready to start sending/receiving once this goroutine is started.
+	err := r.SetMode(RF95W_MODE_RXCONTINUOUS)
+	if err != nil {
+		fmt.Printf("queueHandler() can't set receive mode: %s\n", err.Error())
+		return
+	}
 
+	txWaiting = make([][]byte, 0)
+	for {
+		select {
+		case <-r.interruptChan:
+			// Get the IRQ flags.
+			irqFlags := r.GetRegister(0x12)
+			fmt.Printf("queueHandler() interrupt received, currentMode=%02x, irqFlags=%02x\n", currentMOde, irqFlags)
+			switch r.currentMode {
+			case RF95W_MODE_TX:
+				if irqFlags&RF95W_IRQ_FLAG_TXDONE != 0 {
+					// TX finished.
+					fmt.Printf("queueHandler() transmit finished.\n")
+					// Are there more messages that we need to send? Always empty the queue before starting to receive.
+					if len(txWaiting) > 0 {
+						fmt.Printf("queuehandler() starting new transmission.\n")
+						// Switch to transmit mode (again).
+						err := r.sendMessage(txWaiting[0])
+						if err != nil {
+							fmt.Printf("queueHandler() send message error: %s\n", err.Error())
+						} else {
+							txWaiting = txWaiting[1:] // Message was buffered to the radio successfully.
+						}
+					} else {
+						// No more messages waiting to transmit, go back to receive mode.
+						r.SetMode(RF95W_MODE_RXCONTINUOUS)
+					}
+				}
+			case RF95W_MODE_RXCONTINUOUS:
+				if irqFlags&RF95W_IRQ_FLAG_RXTIMEOUT != 0 {
+					// Timeout. Do nothing, since we're receiving in continuous mode.
+				} else if irqFlags&RF95W_IRQ_FLAG_PAYLOADCRCERROR != 0 {
+					fmt.Printf("queueHandler() received packet with CRC error. discarding.\n")
+				} else if irqFlags&RF95W_IRQ_FLAG_RXDONE != 0 {
+					fmt.Printf("queueHandler() received RXDONE.\n")
+					// Get the total length of the packet.
+					len, err := r.GetRegister(0x13)
+					if err != nil {
+						fmt.Printf("queueHandler() fatal error receiving packet, can't get length: %s\n", err.Error())
+						continue
+					}
+					// Get the start address in the FIFO queue.
+					fifoPtr, err := r.GetRegister(0x10)
+					if err != nil {
+						fmt.Printf("queueHandler() fatal error receiving packet, can't get start pointer address: %s\n", err.Error())
+						continue
+					}
+					// Set the read address to the start of the message in the FIFO queue.
+					err = r.SetRegister(0x0D, fifoPtr)
+					if err != nil {
+						fmt.Printf("queueHandler() fatal error receiving packet, can't set FIFO pointer: %s\n", err.Error())
+						continue
+					}
+					// Read the data.
+					msgBuf, err := r.GetBytes(0x00, int(len))
+					if err != nil {
+						fmt.Printf("queueHandler() fatal error receiving packet, can't read FIFO buffer: %s\n", err.Error())
+						continue
+					}
+					// Get some extra stats - SNR, RSSI, etc.
+					snrByte, _ := r.GetRegister(0x19)
+					rssiByte, _ := r.GetRegister(0x1A)
+					//FIXME: Converting snr should be easier.
+					rdr := bytes.NewReader([]byte{snrByte})
+					var snr int8
+					binary.Read(buf, binary.LittleEndian, &snr)
+					var newMessage RFM95W_Message
+					newMessage.SNR = float64(snr) / 4.0
+					newMessage.RSSI = int(rssiByte) - 137
+					newMessage.Buf = msgBuf
+					newMessage.Received = time.Now()
+					fmt.Printf("Message: %v\n", newMessage)
+					r.RecvBuf = append(r.RecvBuf, newMessage)
+				}
+			}
+			// Clear the IRQ flags.
+			r.SetRegister(0x12, 0xFF)
+		case msg := <-r.txQueue:
+			txWaiting = append(txWaiting, msg) // txWaiting is a FIFO queue.
+			if len(txWaiting) > MAX_TXQUEUE_PILEUP {
+				// Too many messages are in the queue. Start dropping the oldest.
+				fmt.Printf("WARNING: queueHandler() dropping oldest messages, %d in queue.\n", len(txWaiting))
+				txWaiting = txWaiting[len(txWaiting)-MAX_TXQUEUE_PILEUP:]
+			}
+			if r.currentMode != RF95W_MODE_TX { // If we're currently in TX mode, let the current transmission finish.
+				fmt.Printf("queuehandler() starting new transmission.\n")
+				// Switch to transmit mode.
+				err := r.sendMessage(txWaiting[0])
+				if err != nil {
+					fmt.Printf("queueHandler() send message error: %s\n", err.Error())
+				} else {
+					txWaiting = txWaiting[1:] // Message was buffered to the radio successfully.
+				}
+			}
+		case <-r.shutdownQueueHandler:
+			fmt.Printf("queueHandler() received shutdown.\n")
+			r.SetMode(RF95W_MODE_STDBY)
+			return
+		}
+	}
 }
